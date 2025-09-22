bb09ffbd0d3bf72500b176f33978a44d79c6dab5
Tuhin Mallick <tuhin.mllk@gmail.com>
2025-09-22 19:24:35 +0200

    Remove unused CDN configuration from next.config.js to streamline settings and improve maintainability.

--- next.config.js
+++ next.config.js
@@ -71,11 +71,6 @@ const nextConfig = {
 		},
 		{
 		  protocol: 'https',
-		  hostname: 'cdn.sanity.io',
-		  port: '',
-		},
-		{
-		  protocol: 'https',
 		  hostname: 'lh3.googleusercontent.com',
 		  port: '',
 		},
